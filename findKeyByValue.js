641983ea3bb417367aefa378e82a9d344ff3f11c
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-08-19 22:39:11 +0000

    add findKeyByValue.js, run eslint --fix

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(bestTVShowsByGenre, tvShow) {
+  //use object.keys to generate an array of all the keys
+  let genreList = (Object.keys(bestTVShowsByGenre));
+  console.log(genreList);
+  //console.log(genreList);
+  for (let genre of genreList) {
+    if (bestTVShowsByGenre[genre] === tvShow) {
+      console.log(genre);
+      return genre;
+    }
+  }
+};
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"); //PASS
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); //PASS
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), undefined); //FAIL
