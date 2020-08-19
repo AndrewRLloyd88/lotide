bc36b740e5ad99fa2506518716acbbd0d1653d04
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-08-19 01:41:31 +0000

    add eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  actual === expected ? console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0, array1Length = array1.length; i < array1Length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
