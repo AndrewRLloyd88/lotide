e495270349f4c487781c7ba84ae7e2ebf1fb9bb9
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-08-19 15:45:33 +0000

    eslint fix on assertArraysEqual.js and fixed the without.js file to use includes method. The function checks if the second array does not include the word from itemstremove and pushes anything in the source that isn't in itemstoremove. eslint fixes also applied to without.

--- without.js
+++ without.js
@@ -1,7 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -15,25 +11,33 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
-const without = function (source, itemsToRemove) {
-  newWords = [];
+
+const without = function(source, itemsToRemove) {
+  const newWords = [];
   source.forEach(function(v) {
-    if(!itemsToRemove.includes(v)) {
-    newWords.push(v)
+    if (!itemsToRemove.includes(v)) {
+      newWords.push(v);
     }
   });
   
   return newWords;
-}
+};
 
 // test cases
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-// const words = ["hello", "world", "lighthouse", "qwerty", 1, 19, "pike", true];
-// console.log(without(words, ["lighthouse", "alter", "life", "escudo", 1, 2, "world"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
+// const words = ["hello", "world", "lighthouse"];
+// console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
 // assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+const words = ["hello", "world", "lighthouse", "qwerty", 1, 19, "pike", true];
+console.log(without(words, ["lighthouse", "alter", "life", "escudo", 1, 2, "world"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse", "qwerty", 1, 19, "pike", true]);
