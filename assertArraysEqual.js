72648251bb5e968a3c734f4adb1202e5154ce6b7
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-08-19 15:39:56 +0000

    integrated eqArrays into assertArraysEqual and fixed bug not reading array.length in eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
-};
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -14,6 +11,18 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 };
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
 
 //test cases
 assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
--- without.js
+++ without.js
@@ -1,37 +1,39 @@
-const assertArraysEqual = function (actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   actual === expected ? console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
 };
 
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
     return false;
-  }
-
-  for (let i = 0, array1Length = array1.length; i < array1Length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
   }
   return true;
 };
 
+
 const without = function (source, itemsToRemove) {
   newWords = [];
-  for (let i = 0, sourceLength = source.length; i < sourceLength; i++) {
-    if (source[i] !== itemsToRemove[i]) {
-      newWords.push(source[i])
+  source.forEach(function(v) {
+    if(!itemsToRemove.includes(v)) {
+    newWords.push(v)
     }
-  }
+  });
+  
   return newWords;
 }
 
-test cases
+// test cases
 const words = ["hello", "world", "lighthouse"];
 console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
-Make sure the original array was not altered by the without function
+// Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
-const words = ["hello", "world", "lighthouse", "qwerty", 1, 19, "pike", true];
-console.log(without(words, ["lighthouse", "alter", "life", "escudo", 1, 2, "world"])); // no need to capture return value for this test case
+// const words = ["hello", "world", "lighthouse", "qwerty", 1, 19, "pike", true];
+// console.log(without(words, ["lighthouse", "alter", "life", "escudo", 1, 2, "world"])); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 // assertArraysEqual(words, ["hello", "world", "lighthouse"]);
