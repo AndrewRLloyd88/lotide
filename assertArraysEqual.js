6e97c093c7e0c7e03f755859ab18beda29066c77
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-08-24 18:42:50 +0000

    made one cleanup to eqArryays.js and broke assertArraysEqual.js into its own test file in test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,6 @@
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -20,15 +10,8 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
+module.exports = assertArraysEqual;
 
 
 
 
-//test cases
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
-
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,14 @@
 const assertEqual = require('./assertEqual');
 
+//checks if two arrays are strictly and completley equal
 const eqArrays = function(array1, array2) {
+  //if the lengths of both arrays don't match arrays aren't equal
   if (array1.length !== array2.length) {
     return false;
   }
 
   //the loop does things for as long as i < array1.length
-  for (let i = 0, array1Length = array1.length; i < array1Length; i++) {
+  for (let i = 0; i < array1.length; i++) {
     //if array1[i] doesn't match array2[i] then they aren't equal
     if (array1[i] !== array2[i]) {
       return false;
