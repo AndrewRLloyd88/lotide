edddc94373eb7beeb4ebc54d7e5f179b2616e32d
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-08-24 21:41:58 +0000

    broke tail and middle tests out into own files test/middleTest.js and tailTest.js import middle and tail and assert.deepEqual to each file. We are testing deep equaity between objects. all files eslint--fix applied.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,24 +1,52 @@
+const assert = require('chai').assert.deepEqual;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
 
-//test cases
+
+//chai and mocha testing
 // For arrays with one or two elements, there is no middle. Return an empty array.
-const result = middle([1]); // => []
-assertArraysEqual(result, []);
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    const result = middle([1]);
+    const expected = [];
+    assert(result, expected);
+  });
 
-const result2 = middle([1, 2]); //should return empty array
-assertArraysEqual(result2, []); // [] === []
+  it("returns [] for [1, 2]", () => {
+    const result2 = middle([1, 2]);
+    const expected2 = [];
+    assert(result2, expected2);
+  });
 
-const result3 = middle([1, 2, 3]); //should output [2]
-assertArraysEqual(result3, [2]); //[2] === [2]
+  //for arrays with an odd number of elements greater than 2 - the middle element should be returned
 
-const result4 = middle([1, 2, 3, 4, 5]); //[3]
-assertArraysEqual(result4, [3]); //[3] === [3]
+  it("returns [2] for [1, 2, 3]]", () => {
+    const result3 = middle([1, 2, 3]);
+    const expected3 = [2];
+    assert(result3, expected3);
+  });
 
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
 
-const result5 = middle([1, 2, 3, 4]); //[2, 3]
-assertArraysEqual(result5, [2, 3]); //[2, 3] === [2, 3]
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    const result4 = middle([1, 2, 3, 4, 5]);
+    const expected4 = [3];
+    assert(result4, expected4);
+  });
 
-const result6 = middle([1, 2, 3, 4, 5, 6]); //[3, 4]
-assertArraysEqual(result6, [3, 4]); //[3, 4] === [3, 4]
+
+  // For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+  
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    const result5 = middle([1, 2, 3, 4]);
+    const expected5 = [2, 3];
+    assert(result5, expected5);
+  });
+
+
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    const result6 = middle([1, 2, 3, 4, 5, 6]); //[3, 4]
+    const expected6 = [3, 4];
+    assert(result6, expected6);
+  });
+
+});
